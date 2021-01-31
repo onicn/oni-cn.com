70b6711965bc940650161dc32c8006733ddfdc54
falood <falood@gmail.com>
2021-01-31 13:52:46 -0800

    add homepage

--- lib/onicn.ex
+++ lib/onicn.ex
@@ -5,5 +5,38 @@ defmodule Onicn do
     Onicn.Categories.Gas.generate_pages()
     Onicn.Category.generate_json()
     Onicn.Category.generate_pages()
+    generate_home()
+  end
+
+  def generate_home do
+    temp_path =
+      :onicn
+      |> :code.priv_dir()
+      |> Path.join("templates")
+
+    nav =
+      temp_path
+      |> Path.join("nav.eex")
+      |> EEx.eval_file(nav: "home")
+
+    container =
+      temp_path
+      |> Path.join("home.eex")
+      |> EEx.eval_file([])
+
+    script = ~s|layui.use('element', function() {});|
+
+    page =
+      temp_path
+      |> Path.join("index.eex")
+      |> EEx.eval_file(nav: nav, container: container, script: script)
+
+      page_path =
+        :onicn
+        |> :code.priv_dir()
+        |> Path.join("dist")
+
+      File.mkdir_p!(page_path)
+      File.write!(Path.join(page_path, "index.html"), page)
   end
 end
