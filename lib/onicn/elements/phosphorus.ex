9d6a31684b0e4b46868aa481a700cd1e679730c4
Luna <programiao@gmail.com>
2021-01-31 23:46:44 -0800

    Update phosphorus.ex

--- lib/onicn/elements/phosphorus.ex
+++ lib/onicn/elements/phosphorus.ex
@@ -1,4 +1,10 @@
 defmodule Onicn.Elements.Phosphorus do
   use Onicn.Categories.Solid,
     cn_name: "固态磷"
+  
+  summary "固态磷是一种消耗性矿石。它不是游戏世界中的天然矿物，需要人工制备才能获得。"
+
+  usage "固态磷可以作为饲料喂食给天蓝虫和深渊虫。使用固态磷作为诱饵建造飞行动物诱饵可以吸引天蓝虫和深渊虫。"
+
+  production "先把磷矿熔化得到液态磷，再将液态磷冷却凝固就得到固态磷。"
 end
