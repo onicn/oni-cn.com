bb2237835f0f87c23000afd22830e897b6496832
Luna <programiao@gmail.com>
2021-02-02 01:35:49 -0800

    Update ceramic.ex

--- lib/onicn/elements/ceramic.ex
+++ lib/onicn/elements/ceramic.ex
@@ -1,4 +1,11 @@
 defmodule Onicn.Elements.Ceramic do
   use Onicn.Categories.Solid,
     cn_name: "陶瓷"
+    
+  summary "陶瓷是一种人造的隔热建筑材料，游戏中期以后可以制造。"
+  
+  usage "作为一种建筑材料，陶瓷使建筑的过热温度增加200摄氏度，并使得装饰度增加20%。陶瓷的导热率较低，可以减缓热传递。"
+  usage "碎石机能够将陶瓷粉碎成沙子。100 千克 陶瓷 = 100 千克 沙子。"
+  
+  production "窑炉能够用粘土和煤炭烧制陶瓷。100 千克 粘土 + 25 千克 煤炭 = 100 千克 陶瓷。"
 end
