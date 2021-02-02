d5479fb81fc586816da8fd5aa51e02c801d3c3d1
Luna <programiao@gmail.com>
2021-02-01 23:04:59 -0800

    Update aluminum_ore.ex

--- lib/onicn/elements/aluminum_ore.ex
+++ lib/onicn/elements/aluminum_ore.ex
@@ -1,4 +1,11 @@
 defmodule Onicn.Elements.AluminumOre do
   use Onicn.Categories.Solid,
     cn_name: "铝矿"
+    
+    summary "铝矿是一种金属矿石，常见于森林区。它是可以挖到的比热容和导热率最高的矿石。"
+    
+    usage "作为一种金属矿石，铝矿可用于建造各种建筑。"
+    usage "金属精炼器可以将铝矿精炼成铝。100 千克 铝矿 = 100 千克 铝。"
+    usage "当将铝矿喂食给光滑哈奇时，光滑哈奇会排泄出铝。此法约有75%的转化率。"
+    usage "碎石机可以将铝矿粉碎成铝和沙子。100 千克 铝矿 = 50 千克 铝 + 50 千克 沙子。这个方法转化率太低不建议使用。"
 end
