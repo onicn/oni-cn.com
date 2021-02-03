a202843dc7f30f490025c83d27c6967697ed8af4
falood <falood@gmail.com>
2021-02-03 11:10:38 -0800

    format

--- lib/onicn/elements/liquid_carbon_dioxide.ex
+++ lib/onicn/elements/liquid_carbon_dioxide.ex
@@ -1,8 +1,8 @@
 defmodule Onicn.Elements.LiquidCarbonDioxide do
   use Onicn.Categories.Liquid,
     cn_name: "液态二氧化碳"
-    
+
   summary "液态二氧化碳是二氧化碳的液体形态。"
-  
+
   production "二氧化碳间歇泉定期喷出液态二氧化碳。一般情况下，这些液态二氧化碳会迅速气化变成二氧化碳。"
 end
