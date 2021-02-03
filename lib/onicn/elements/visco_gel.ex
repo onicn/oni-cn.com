6a15c314ceaa8c7c654b2aeb363476d70517b09d
Luna <programiao@gmail.com>
2021-02-03 00:33:34 -0800

    Update visco_gel.ex

--- lib/onicn/elements/visco_gel.ex
+++ lib/onicn/elements/visco_gel.ex
@@ -1,4 +1,10 @@
 defmodule Onicn.Elements.ViscoGel do
   use Onicn.Categories.Liquid,
     cn_name: "粘性凝胶"
+    
+  summary "粘性凝胶是一种特殊的液体。和其他液体不同，粘性凝胶不会水平流动，而是会形成近似每格100千克的竖直排列。"
+  
+  usage "利用粘性凝胶的上述特性，玩家可以实现许多特殊设计。经常用来制作水门隔绝液体两边的气体交换。"
+  
+  production "分子熔炉能够用异构树脂和石油合成粘性凝胶。35 千克 异构树脂 + 65 千克 石油 = 100 千克 粘性凝胶。"
 end
