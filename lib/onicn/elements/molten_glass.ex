71dc294b14b2aaad863ae69c37c4f604477c89d6
Luna <programiao@gmail.com>
2021-02-03 23:17:26 -0800

    Update molten_glass.ex

--- lib/onicn/elements/molten_glass.ex
+++ lib/onicn/elements/molten_glass.ex
@@ -1,4 +1,10 @@
 defmodule Onicn.Elements.MoltenGlass do
   use Onicn.Categories.Liquid,
     cn_name: "熔融玻璃"
+    
+  summary "熔融玻璃是玻璃的液体形态。"
+  
+  usage "将熔融玻璃冷却就得到玻璃。"
+  
+  production "玻璃熔炉能够将沙子融化成熔融玻璃。100 千克 沙子 = 25 千克 熔融玻璃。建议使用尽可能短的隔热液体管道，以便预防熔融玻璃在管道中冷却变成玻璃而损坏管道。"
 end
