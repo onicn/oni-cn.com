542c48d855efea9f1131ff20d3fdfa7ca7d29a4d
Luna <programiao@gmail.com>
2021-02-03 23:27:10 -0800

    Update naphtha.ex

--- lib/onicn/elements/naphtha.ex
+++ lib/onicn/elements/naphtha.ex
@@ -1,4 +1,9 @@
 defmodule Onicn.Elements.Naphtha do
   use Onicn.Categories.Liquid,
     cn_name: "石脑油"
+    
+  summary "石脑油是塑料熔化后的产物。"
+  
+  usage "石脑油可以继续加热转化为酸性天然气回收利用。"
+  usage "导热率较低可以用于隔热。"
 end
