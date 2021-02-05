6a62202eb03d26d97971ab0f0e88bfe767a1bf3e
Luna <programiao@gmail.com>
2021-02-04 22:27:47 -0800

    Update fullerene.ex

--- lib/onicn/elements/fullerene.ex
+++ lib/onicn/elements/fullerene.ex
@@ -1,4 +1,8 @@
 defmodule Onicn.Elements.Fullerene do
   use Onicn.Categories.Solid,
     cn_name: "富勒烯"
+    
+  summary "富勒烯是一种稀有资源，只能自太空任务中获得。"
+  
+  usage "分子熔炉能够用富勒烯、金和石油合成超级冷却剂。1 千克 富勒烯 + 49.5 千克 金 + 49.5 千克 石油 = 100 千克 超级冷却剂。"
 end
