f0bc73ab6dcdd1b134ad875fe38940672766733e
Luna <programiao@gmail.com>
2021-02-04 22:50:12 -0800

    Update lead.ex

--- lib/onicn/elements/lead.ex
+++ lib/onicn/elements/lead.ex
@@ -1,4 +1,10 @@
 defmodule Onicn.Elements.Lead do
   use Onicn.Categories.Solid,
     cn_name: "铅"
+    
+  summary "铅是一种精炼金属，天然存在于原油区。"
+  
+  usage "作为一种建筑材料，铅可以用来建造各种先进电力系统、建筑和自动化系统。由于铅会降低建筑的过热温度，它不适合建造发电机或电池，而更适合建造导线和自动化系统。"
+  
+  production "在原油区有大量天然存在的铅。"
 end
