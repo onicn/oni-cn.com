7e79675b910bd834a535180869cb3e3fec4ea036
Luna <programiao@gmail.com>
2021-02-05 21:09:32 -0800

    Update rust.ex

--- lib/onicn/elements/rust.ex
+++ lib/onicn/elements/rust.ex
@@ -1,4 +1,8 @@
 defmodule Onicn.Elements.Rust do
   use Onicn.Categories.Solid,
     cn_name: "铁锈"
+    
+  summary "铁锈是一种消耗性矿石，常见于铁锈区。"
+  
+  usage "铁锈脱氧机能够用铁锈和盐制氧。750 克/秒 铁锈 + 250 克/秒 盐 = 570 克/秒 氧气 + 30 克/秒 氯气 + 400 克/秒 铁矿。"
 end
