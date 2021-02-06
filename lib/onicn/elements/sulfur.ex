e64f2cefd06687bde4a753c5347a175b11df863f
Luna <programiao@gmail.com>
2021-02-05 21:30:07 -0800

    Update sulfur.ex

--- lib/onicn/elements/sulfur.ex
+++ lib/onicn/elements/sulfur.ex
@@ -1,4 +1,10 @@
 defmodule Onicn.Elements.Sulfur do
   use Onicn.Categories.Solid,
     cn_name: "硫"
+    
+  summary "硫是由酸性天然气制备液态甲烷的副产品。"
+  
+  usage "硫暂无实际用途。"
+  
+  production "将酸性天然气冷却至-163.5摄氏度（凝点-2摄氏度）时会得到液态甲烷（67%质量）和硫（33%质量）。"
 end
