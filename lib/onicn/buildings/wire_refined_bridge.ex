508e8c05834263b2da8b508e3db61ac3662a5ab1
Luna <programiao@gmail.com>
2021-02-09 12:24:29 -0800

    Update wire_refined_bridge.ex

--- lib/onicn/buildings/wire_refined_bridge.ex
+++ lib/onicn/buildings/wire_refined_bridge.ex
@@ -1,3 +1,11 @@
 defmodule Onicn.Buildings.WireRefinedBridge do
   use Onicn.Categories.Building
+  
+  summary "导线桥是电线桥的高级版本，和导线配套使用。"
+  summary "导线桥可以连接左右两格的导线，而跨过中间一格。"
+  summary "导线桥也可旋转至竖直位置。"
+
+  usage "导线的负载功率上限是2千瓦。"
+  usage "将用电器分割成不同电网时，有时需要用到导线桥。"
+
 end
