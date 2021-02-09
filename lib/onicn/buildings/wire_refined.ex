20daa33c0f9ef0cb0e37d2428086634c4aa79980
Luna <programiao@gmail.com>
2021-02-09 12:25:46 -0800

    Update wire_refined.ex

--- lib/onicn/buildings/wire_refined.ex
+++ lib/onicn/buildings/wire_refined.ex
@@ -1,3 +1,8 @@
 defmodule Onicn.Buildings.WireRefined do
   use Onicn.Categories.Building
+  
+  summary "导线是电线的高级版本，可以负载2千瓦功率。"
+  summary "导线可以负载2千瓦功率，而且没有装饰值处罚。"
+
+  usage "连接实时负载不超过2千瓦的电网。"
 end
