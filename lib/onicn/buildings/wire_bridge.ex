a1652f1f0bc15d974a7c136e71521414197cf4ed
Luna <programiao@gmail.com>
2021-02-09 12:19:38 -0800

    Update wire_bridge.ex

--- lib/onicn/buildings/wire_bridge.ex
+++ lib/onicn/buildings/wire_bridge.ex
@@ -1,3 +1,8 @@
 defmodule Onicn.Buildings.WireBridge do
   use Onicn.Categories.Building
+  
+  summary "电线桥用于让一根电线跨越另一根电线而不相互连接。"
+
+  usage "电线的负载功率上限是1千瓦。"
+  usage "将用电器分割成不同电网时，有时需要用到电线桥。"
 end
