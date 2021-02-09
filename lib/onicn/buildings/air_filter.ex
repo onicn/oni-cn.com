2a4649b3d6fd714ae508e49c20a310ad32673b47
Luna <programiao@gmail.com>
2021-02-09 11:27:02 -0800

    Update air_filter.ex

--- lib/onicn/buildings/air_filter.ex
+++ lib/onicn/buildings/air_filter.ex
@@ -1,3 +1,11 @@
 defmodule Onicn.Buildings.AirFilter do
   use Onicn.Categories.Building
+  
+  summary "空气净化器能够将污染氧净化为氧气。"
+  summary "污染氧的作用范围是左右各三格，上下各两格（不含空气净化器本身格子）。"
+  summary "污染氧中携带的黏液肺病菌大部分转移到产出的粘土中，少部分转移到产出的氧气中。"
+  summary "这两者中黏液肺均不能存活，会快速死亡。"
+
+  usage "用于清除沼泽区中的大量污染氧，以及其他污染氧来源。"
+
 end
