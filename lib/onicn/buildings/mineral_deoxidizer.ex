a21f8b8603b093c52feff391fb236657e7931e56
Luna <programiao@gmail.com>
2021-02-09 11:40:29 -0800

    Update mineral_deoxidizer.ex

--- lib/onicn/buildings/mineral_deoxidizer.ex
+++ lib/onicn/buildings/mineral_deoxidizer.ex
@@ -1,3 +1,13 @@
 defmodule Onicn.Buildings.MineralDeoxidizer do
   use Onicn.Categories.Building
+  
+  summary "氧气扩散器是最早的生产氧气的建筑。"
+  summary "氧气扩散器消耗藻类生成氧气。"
+  summary "当周围气压达到1800克时，氧气扩散器会因气压过高而暂停工作。"
+  summary "藻类中含有的黏液肺病菌会一并转移到氧气中，但这通常不是太大问题，因为病菌会在氧气中死亡。"
+  summary "生成的氧气固定为30摄氏度。"
+
+  usage "在游戏早期，氧气扩散器是主要的供氧方式。"
+  usage "氧气扩散器对藻类需求很大，玩家需要适时切换到电解水供氧的方式。"
+
 end
