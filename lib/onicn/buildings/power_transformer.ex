9069a2e2bf75a2b5e694648e53f03d1a3959605e
Luna <programiao@gmail.com>
2021-02-09 12:15:19 -0800

    Update power_transformer.ex

--- lib/onicn/buildings/power_transformer.ex
+++ lib/onicn/buildings/power_transformer.ex
@@ -1,3 +1,14 @@
 defmodule Onicn.Buildings.PowerTransformer do
   use Onicn.Categories.Building
+
+  summary "大功率转变器是电力变压器的高级版本，可以支持4千瓦的低负载电网。"
+  summary "参见功率转变器。"
+  summary "大功率转变器的低负载端通常连接导电线。"
+  summary "虽然大功率转变器可以支持4千瓦负载，但导电线只能支持2千瓦负载。"
+
+  usage "连接高负载电网和低负载电网（限制为2千瓦）。"
+
+  tips "电池应放置于高负载电网，而非低负载电网。"
+  tips "电池充电速率无限制，会导致低负载电网迅速过载。"
+
 end
