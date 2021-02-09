2bb77910229388d9def753efe7a792ca60a80db7
Luna <programiao@gmail.com>
2021-02-09 11:45:56 -0800

    Update battery.ex

--- lib/onicn/buildings/battery.ex
+++ lib/onicn/buildings/battery.ex
@@ -1,3 +1,11 @@
 defmodule Onicn.Buildings.Battery do
   use Onicn.Categories.Building
+  
+  summary "电池用来储存发电机的多余电力，初始即可建造。"
+  summary "当发电机产生的电力大于电器消耗的电力时，同一电网的电池可以储存多余电力，上限10千焦。"
+  summary "当发电机产生的电力小于电器消耗的电力时，电池储存的电力可以供给电网。"
+  summary "储存在电池中的电力会自然流失，速率为每周期1千焦。"
+
+  usage "在游戏早期储存多余电力。"
+
 end
