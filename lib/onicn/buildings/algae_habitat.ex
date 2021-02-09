f92ec7524ab95d2497f41a07c3fa90bd04982b24
Luna <programiao@gmail.com>
2021-02-09 11:29:33 -0800

    Update algae_habitat.ex

--- lib/onicn/buildings/algae_habitat.ex
+++ lib/onicn/buildings/algae_habitat.ex
@@ -1,3 +1,17 @@
 defmodule Onicn.Buildings.AlgaeHabitat do
   use Onicn.Categories.Building
+  
+
+  summary "藻类箱能够产生氧气并清除少量二氧化碳。"
+  summary "藻类箱需要定期供给藻类和水。"
+  summary "藻类箱产生氧气的功能和清除二氧化碳的功能是互相独立的，环境气体并非二氧化碳的情况下它也依然可以产生氧气。"
+  summary "藻类箱产生的污水会以瓶装的方式堆积在地面上。"
+  summary "使用一段时间后，藻类箱会需要复制人维护一下。"
+  summary "藻类箱产生的氧气固定为30摄氏度。"
+
+  usage "在竖直通道下部距离较远处建造藻类箱可以让复制人换气，避免来回长途跋涉。"
+
+  tips "灯、屋顶光照、发光虫、印刷舱都可以作为光照来源。"
+
+
 end
