ef8b0f5944b703f8a13a0be819beade7f8affeff
Xiangrong Hao <falood@gmail.com>
2021-02-17 21:16:36 -0800

    Update lib/onicn/buildings/steam_turbine2.ex

--- lib/onicn/buildings/steam_turbine2.ex
+++ lib/onicn/buildings/steam_turbine2.ex
@@ -1,17 +1,3 @@
 defmodule Onicn.Buildings.SteamTurbine2 do
   use Onicn.Categories.Building
-
-  section "表格测试" do
-    "其它简介"
-
-    """
-    | 蒸汽吸入口数量 | 蒸汽温度(°C) | 功率(W) |
-    | -------------- | ------------ | ------- |
-    | 5              | 200          | 850     |
-    | 4              | 226.25       | 850     |
-    | 3              | 270          | 850     |
-    | 2              | 357.5        | 850     |
-    | 1*             | 357.5        | 425     |
-    """
-  end
 end
