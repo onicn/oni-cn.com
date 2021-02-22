09928e7b26f420ffeaaafe1c936e0577cbfad182
programiao <programiao@gmail.com>
2021-02-22 01:48:22 -0800

    add refinement description

--- lib/onicn/buildings/oil_refinery.ex
+++ lib/onicn/buildings/oil_refinery.ex
@@ -1,3 +1,14 @@
 defmodule Onicn.Buildings.OilRefinery do
   use Onicn.Categories.Building
+
+  section "简介" do
+    "原油精炼器可以将原油以2:1的比例转化为石油，并释放一些天然气到环境中。"
+    "周围环境气压高于5千克后停止工作。"
+    "需要复制人操作。"
+  end
+
+  section "用途" do
+    "将原油转化为石油和天然气，石油可以用来供给石油发电机发电，或聚合压机生产塑料。"
+  end
+
 end
--- lib/onicn/buildings/rock_crusher.ex
+++ lib/onicn/buildings/rock_crusher.ex
@@ -1,3 +1,16 @@
 defmodule Onicn.Buildings.RockCrusher do
   use Onicn.Categories.Building
+
+  section "简介" do
+    "碎石机可以用矿物原料和金属矿石生产沙子和精炼金属，也可以生产食盐和石灰。"
+    "需要复制人操作。"
+  end
+
+  section "用途" do
+    "生产食盐和石灰，在前期生产精炼金属。"
+  end
+
+  section "小技巧" do
+    "碎石机金属矿石到精炼金属的转化率只有50%，用金属精炼器可以产生更多精炼金属。由于炼钢仍然需要碎石机生产石灰，因此金属精炼器不能完全替代碎石机。"
+  end
 end
