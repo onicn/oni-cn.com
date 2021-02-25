b371a63ceddc5fb64540fc8b940d05bce5b74669
Xiangrong Hao <falood@gmail.com>
2021-02-24 23:21:26 -0800

    add plants (#19)
    * add plants
    
    * fix spice vine cn name

---
+++ lib/onicn/plants/cold_wheat.ex
@@ -0,0 +1,8 @@
+defmodule Onicn.Plants.ColdWheat do
+  use Onicn.Categories.Plant,
+    cn_name: "冰霜小麦",
+    seed_cn_name: "冰霜麦粒",
+    en_name: "Sleet Wheat",
+    seed_en_name: "Sleet Wheat Grain",
+    seed: "cold_wheat_seed"
+end
---
+++ lib/onicn/plants/oxyfern.ex
@@ -0,0 +1,8 @@
+defmodule Onicn.Plants.Oxyfern do
+  use Onicn.Categories.Plant,
+    cn_name: "氧齿蕨",
+    seed_cn_name: "氧齿蕨种子",
+    en_name: "Oxyfern",
+    seed_en_name: "Oxyfern Seed",
+    seed: "oxyfern_seed"
+end
