51b72b5bd09a4557d974d05797d3216aabaf5ac2
falood <falood@gmail.com>
2021-02-24 23:19:38 -0800

    fix spice vine cn name

--- lib/onicn/plants/spice_vine.ex
+++ lib/onicn/plants/spice_vine.ex
@@ -1,6 +1,6 @@
 defmodule Onicn.Plants.SpiceVine do
   use Onicn.Categories.Plant,
-    cn_name: "火椒",
+    cn_name: "火椒藤",
     seed_cn_name: "火椒种子",
     en_name: "Pincha Pepper",
     seed_en_name: "Pincha Pepper Seed",
