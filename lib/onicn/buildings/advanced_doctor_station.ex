f74ab68cbe523722089d4d2603842af5b4dc3bdc
Mapleaf16 <xinyuye2018@outlook.com>
2021-02-27 17:58:00 +0800

    添加病诊站，超级计算机，温度调节器，机器人矿工的中文翻译
    部分需要完善的地方（如添加表格，更换为官方汉化）已在文件中注释

--- lib/onicn/buildings/advanced_doctor_station.ex
+++ lib/onicn/buildings/advanced_doctor_station.ex
@@ -1,3 +1,7 @@
 defmodule Onicn.Buildings.AdvancedDoctorStation do
   use Onicn.Categories.Building
+  #官方英文名为"Disease Clinic"
+  #僵尸孢子造成的疾病名称?
+  summary "病诊站专门用于治疗僵尸孢子造成的疾病"
+  summary "治疗需要消耗1kg血清素以及拥有高级医疗护理技能的复制人进行"
 end
