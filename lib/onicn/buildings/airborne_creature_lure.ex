ae2f62528c5e15d1f8bccef668a547b96a5011ba
mochamapleaf <57422783+mochamapleaf@users.noreply.github.com>
2021-02-28 00:41:57 +0800

    补充了全部'A'开头的建筑资料 (#22)
    Todo: 
    校对超级计算机
    删除airborne_creature_lure.ex以及astronaut_training_center.ex

--- lib/onicn/buildings/airborne_creature_lure.ex
+++ lib/onicn/buildings/airborne_creature_lure.ex
@@ -1,3 +1,4 @@
 defmodule Onicn.Buildings.AirborneCreatureLure do
   use Onicn.Categories.Building
+  #飞行动物诱饵的旧版本未被删除?
 end
--- lib/onicn/buildings/astronaut_training_center.ex
+++ lib/onicn/buildings/astronaut_training_center.ex
@@ -1,3 +1,4 @@
 defmodule Onicn.Buildings.AstronautTrainingCenter do
   use Onicn.Categories.Building
+  #没有找到对应的建筑
 end
