89ef21efd0246f1c6403183f367fb7d40d5dcff3
Luna <programiao@gmail.com>
2021-03-23 01:01:25 -0700

    Add utilities description (#30)
    * Add utilities description
    
    * code format
    
    * reformat table
    
    Co-authored-by: falood <falood@gmail.com>

--- lib/onicn/buildings/exterior_wall.ex
+++ lib/onicn/buildings/exterior_wall.ex
@@ -1,3 +1,9 @@
 defmodule Onicn.Buildings.ExteriorWall do
   use Onicn.Categories.Building
+
+  section "简介" do
+    "干板墙如果建造在太空中可以防止当前格子因太空暴露流失液体和气体。"
+    "不增加装饰度，也不能挡住管道或电线。"
+    "可以建造在有梯子和消防滑杆的格子，但是不能建造在门和砖的后面，因此不能阻止气动门流失气体。"
+  end
 end
