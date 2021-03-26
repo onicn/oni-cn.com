2696e1d738f0eccaa24e58c6dff39953747c452b
programiao <programiao@gmail.com>
2021-03-25 22:39:24 -0700

    Add description for stations

--- lib/onicn/buildings/jet_suit_locker.ex
+++ lib/onicn/buildings/jet_suit_locker.ex
@@ -1,3 +1,7 @@
 defmodule Onicn.Buildings.JetSuitLocker do
   use Onicn.Categories.Building
+
+  section "简介" do
+    "喷气服存放柜和气压服存放柜作用相同，只不过放的是喷气服。"
+  end
 end
--- lib/onicn/buildings/jet_suit_marker.ex
+++ lib/onicn/buildings/jet_suit_marker.ex
@@ -1,3 +1,12 @@
 defmodule Onicn.Buildings.JetSuitMarker do
   use Onicn.Categories.Building
+
+  section "简介" do
+    "喷气服检查站和气压服检查站作用相同，只不过作用于喷气服。"
+  end
+
+  section "BUG" do
+    "复制人会在喷气服检查站这里脱掉气压服丢在地上。"
+    "当有区域可以同时通过气压服检查站和喷气服检查站到达时，复制人偶尔会经过气压服检查站但是不换衣服。"
+  end
 end
