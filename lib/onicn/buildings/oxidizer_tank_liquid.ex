acc85623e7569217f1875e14be35e89a6bad3640
programiao <programiao@gmail.com>
2021-04-01 18:18:48 -0700

    添加火箭建筑描述

--- lib/onicn/buildings/oxidizer_tank_liquid.ex
+++ lib/onicn/buildings/oxidizer_tank_liquid.ex
@@ -1,3 +1,10 @@
 defmodule Onicn.Buildings.OxidizerTankLiquid do
   use Onicn.Categories.Building
+
+  section "简介" do
+    "液态氧化剂舱可以装入液态氧，为液氢引擎或石油引擎提供氧化剂。"
+    "可以替换为固态氧化剂舱和氧石。"
+    "使用液态氧（比氧石）可以使火箭最远飞行距离增加33%。"
+    "可以调整建筑的容量避免输入额外的液态氧造成浪费。多出来的液态氧复制人会过来取出，以瓶装的形式掉出来。"
+  end
 end
