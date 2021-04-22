b9ce869f8e273ac768eb0e93fcb6d59c6fcc76b1
falood <falood@gmail.com>
2021-04-22 00:44:14 -0700

    add crushed rock

---
+++ lib/onicn/elements/crushed_rock.ex
@@ -0,0 +1,12 @@
+defmodule Onicn.Elements.CrushedRock do
+  use Onicn.Categories.Solid,
+    cn_name: "碎岩"
+
+  section "简介" do
+    "碎岩可以自太空任务取得。"
+  end
+
+  section "用途" do
+    "碎岩可以作为饲料喂食给好吃哈奇。"
+  end
+end
