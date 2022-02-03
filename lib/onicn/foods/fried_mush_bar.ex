4d5fee15d3df2a27d2ec37df3a632bc14292cf74
falood <falood@gmail.com>
2022-02-02 22:40:43 -0800

    fix typo

--- lib/onicn/foods/fried_mush_bar.ex
+++ lib/onicn/foods/fried_mush_bar.ex
@@ -1,10 +1,10 @@
 defmodule Onicn.Foods.FriedMushBar do
   use Onicn.Categories.Food,
-    cn_name: "炸泥糕",
+    cn_name: "炸泥膏",
     en_name: "Mush Fry"
 
   section "简介" do
-    "一份炸泥糕需要一份软泥糕作为原料。"
+    "一份炸泥膏需要一份软泥膏作为原料。"
     "如果作为食物的唯一来源，那么每个复制人需要消耗: 泥土 71.43 千克 / 周期，水 71.43 千克 / 周期。"
   end
 end
